7bb1dafccb2e862a79e81c60165522854db7b3df
gmshuvo <gm.shuvo.ru.cse@gmail.com>
2021-05-04 17:54:48 +0600

    making movi card

--- Components/MainContent.js
+++ Components/MainContent.js
@@ -2,7 +2,7 @@ import MoviCard from './MoviCard'
 const MainContent = ({requestData}) => {
    
     return (
-        <div className='grid grid-cols-1 gap-5 px-3 sm:grid-cols-2 md:grid-cols-3 mt-8'>
+        <div className='grid grid-cols-1 gap-5 px-3 sm:grid-cols-2 md:grid-cols-3 mt-8 '>
             {
                 requestData.map((data, key) =>{
                     return (
--- Components/MoviCard.js
+++ Components/MoviCard.js
@@ -1,26 +1,35 @@
 import Image from "next/image";
+import { BiLike } from 'react-icons/bi'
 
 const MoviCard = ({ data }) => {
   console.log(data);
   const base_url = "https://image.tmdb.org/t/p/original";
   return (
-    <div className=" ">
-      <Image
-        className=""
-        layout="responsive"
-        src={
-          `${base_url}${data.backdrop_path} ` ||
-          `${base_url}${data.poster_path} `
-        }
-        objectFit='cover'
-        height={1080}
-        width={1920}
-      />
+    <div className=" group transition duration-200 transform hover:scale-105 overflow-hidden">
+       
+            <Image
+                    className='Relative overflow-hidden'
+                    layout="responsive"
+                    src={
+                    `${base_url}${data.backdrop_path} ` ||
+                    `${base_url}${data.poster_path} `
+                    }
+                    objectFit='cover'
+                    height={1080}
+                    width={1920}
+                />
+        
+            <div className='absolute bg-black bg-opacity-30 opacity-0 w-full h-5/6 top-0 transition delay-100 ease-in transform group-hover:opacity-100 overflow-hidden'>
+            <p className="p-4 text-sm ">{data.overview}</p>
+            </div>
+      
+            
 
-      <div className="mt-1">
-        <h2 className="text-2xl text-white text-bold ">{data.original_title || data.title || data.original_name}</h2>
-        <div>
-          <p className="truncate  ">{data.overview}</p>
+      <div className=" overflow-hidden">
+        <h2 className="text-xl group1-hover:text-white group-hover:text-bold ">{data.original_title || data.title || data.original_name}</h2>
+        <div className='flex items-center mt-1 space-x-1'>
+            <span><BiLike/></span>
+           <h2>popularity: {data.popularity}</h2>         
         </div>
       </div>
     </div>
